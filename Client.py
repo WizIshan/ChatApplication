efa40f16cb52ba164f6c96f7547f71bbce23b11c
PRITISH009 <pritishgupta009@gmail.com>
2019-09-24 22:41:00 +0530

    First Prototype

--- Client.py
+++ Client.py
@@ -1,28 +1,81 @@
 import socket
 from threading import Thread
-import tkinter
+import tkinter, sys
+from distutils.util import strtobool
 
 HOST = "192.168.56.1"
 PORT = 7000
 SERVER_ADDRESS = (HOST, PORT)
 HEADERSIZE = 10
-IPSIZE = 15
 
 def recieve(client):
      while True:
         try:
-            msg = client.recv(1024).decode("utf8")
+            msg = client.recv(1024).decode("utf-8")
             print(msg)
         except OSError:  # Possibly client has left the chat.
             break
 
+def ping(event=None):
+    #Take the input from the ChatRoom GUI
+    reciever = 'Client2'
+    message = 'Hii'
+    message_type = 3
+    if(message == "#Quit"):
+        message_type = 4
+
+    data = f'{message_type}' +f'{len(reciever) :< {HEADERSIZE}}' + f'{reciever}' + f'{len(message) :< {HEADERSIZE}}' + f'{message}'
+    print(data)
+    client.send(bytes(data, 'utf-8'))
+
+def Quit(event=None):
+    #Take the input from the ChatRoom GUI
+    message = '#Quit'
+    message_type = 4
+
+    data = f'{message_type}' + f'{len(message) :< {HEADERSIZE}}' + f'{message}'
+    print(data)
+    client.send(bytes(data, 'utf-8'))
+    print("Disconnection from Server")
+    client.close()
 
 def create_client():
     clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     clientsocket.connect(SERVER_ADDRESS)
     return clientsocket
 
+def Request_Chat_Access(event=None):
+    # button_clicked (if register and then the outcome is false then return user already exists else if login then invalid username/password) (This will be built after the GUI is integrated)
+    user = 'Client1' # To be taken from GUI
+    password = '12345' # To be taken from GUI
+    message_type = 1 # To be decided from either register button or login button
+    message = f'{message_type}' + f'{len(user):<{HEADERSIZE}}' + f'{user}' + f'{len(password) :< {HEADERSIZE}}' + f'{password}'
+    print(message)
+    print('Sending Message')
+    client.send(bytes(message, 'utf-8'))
+    msg = bool(strtobool(client.recv(1024).decode('utf-8')))
+    if msg == True:
+        print('Access Granted')
+        print(msg)
+        return True
+    else :
+        # According to the button clicked the message will be printed to user
+        print('Access Denied')
+        print(msg)
+        return False
+        
+
 if __name__ == '__main__':
     client = create_client()
-    recieve_thread = Thread(target=recieve, args=(client,))
-    recieve_thread.start()
+    #Start Register/Login GUI
+    #Request_Chat_Access is triggered from Login GUI
+    if Request_Chat_Access() is True:
+        #Close the Registeration/Login GUI and move forward with Chat GUI
+        ping()
+        recieve_thread = Thread(target=recieve, args=(client,))
+        recieve_thread.start()
+        x = input()
+        Quit()
+    else:
+        client.close()
+        sys.exit()
--- Server.py
+++ Server.py
@@ -1,28 +1,89 @@
 import socket
 from threading import Thread
-from ast import literal_eval
-import sys
+import os, sys, csv
+from pprint import pprint
+from pymongo import MongoClient
 
 HOST = socket.gethostbyname(socket.gethostname())
 PORT = 7000
 ADDRESS = (HOST, PORT)
 HEADERSIZE = 10
-IPLENGTH = 100
 
-all_address = {}
+GRANT = True
+REVOKE = False
 
-def listen_from_clients(sender,all_address):
-    reciever_ip_len = sender.recv(IPLENGTH).decode('utf-8')
-    reciever_ip = sender.recv(reciever_ip_len).decode('utf-8')
-    message_len = sender.recv(HEADERSIZE).decode('utf-8')
-    message = sender.recv(int(message_len.strip())).decode('utf-8')
+online_users = {}
+database_client = MongoClient('mongodb+srv://Server:Server009@chatcluster-n5m5n.mongodb.net/Users?retryWrites=true&w=majority')
+database = database_client.get_database('Users').user_data
 
-    #reciever = all_address[]
+if database_client.server_info() :
+    print('Connected with database')
+else:
+    print('Database error')
 
-    return
+
+# def disconnect_user(clientsocket, ):
+#     database.update_one({})
+
+def listen_from_clients(sender, online_users):
+    print(f"Listening to {sender}")
+    sender_username = ""
+    for user, value in online_users.items():
+        if value["Socket"] == sender:
+            sender_username = user
+
+    #Check here for if online or not
+    #if online 
+    #pprint(online_users)
+
+    while True:
+        print("Here")
+        message_type = int(sender.recv(1).decode('utf-8'))
+        print("Sender  - ",sender_username)
+        print("Message Type - ", message_type)
+        if message_type == 3:
+            reciever_len = int(sender.recv(HEADERSIZE).decode('utf-8'))
+            reciever = sender.recv(reciever_len).decode('utf-8')
+            message_len = int(sender.recv(HEADERSIZE).decode('utf-8'))
+            message = sender.recv(message_len).decode('utf-8')
+            print(reciever)
+            print(message)
+
+            if reciever in online_users.keys():
+                print("Reciever is Online")
+                send_message(online_users[reciever]['Socket'], message)
+            else :
+                print("Reciever is Offline")
+                send_message(sender, "Reciever is Offline")
+                print("Message Sent")
+        elif message_type == 4:
+            print("Disconnecting Client")
+            database.update_one({"Username" : sender_username}, {"$set" : {"Access" : False}})
+            del online_users[sender_username]
+            sender.close()
+            pprint(online_users)
+            break
+        else:
+            print("Invalid Request Type")
+            break
+
+        
+
+def enter_data(data):
+    database.insert_one(data)
+
+def validate_user(user, password):
+    print("Validating")
+    print(database.count_documents({"$and" : [{"Username" : user}, {"Password" : password}, {"Access" : False}]}))
+
+    if database.count_documents({"$and" : [{"Username" : user}, {"Password" : password}, {"Access" : False}]}) == 1:
+        database.update_one({"$and" : [{"Username" : user}, {"Password" : password}, {"Access" : False}]}, {"$set" : {"Access" : True}})
+        return True
+    return False
+
 
 def send_message(clientsocket, message):
-    clientsocket.send(bytes(message, 'utf-8'))
+    clientsocket.send(bytes(str(message), 'utf-8'))
 
 def Server():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -30,18 +91,49 @@ def Server():
     server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
     return server
 
-def accept_incoming_connections(server,all_address):
+def Grant_Chat_Access(clientsocket, address):
+    message_type = clientsocket.recv(1).decode('utf-8')
+    userlen = int(clientsocket.recv(HEADERSIZE).decode('utf-8').strip())
+    user = clientsocket.recv(userlen).decode('utf-8')
+    passlen = int(clientsocket.recv(HEADERSIZE).decode('utf-8').strip())
+    password = clientsocket.recv(passlen).decode('utf-8')
+    print(message_type)
+    print(user)
+    print(password)
+
+    if message_type == '0' :
+        enter_data({"Username" : user, "Password" : password, "Access" : GRANT})
+        return (True,user)
+
+    elif message_type == '1':
+        status = validate_user(user, password)
+
+        send_message(clientsocket, status)
+
+        return (status,user)
+        
+    else:
+        send_message(clientsocket, 'Invalid Access, Closing Connection')
+        return (False, None)
+
+def accept_incoming_connections(server, online_users):
     while True:
         clientsocket, address = server.accept()
         print(f'Client with {address} has been connected')
-        all_address[address] = {
-            'address' : address,
-            'key' : GenerateKey(),
-            'socket' : clientsocket
-        }
-        print(all_address)
-        send_message(clientsocket, 'Connection Established')
-        Thread(target=listen_from_clients, args=(clientsocket, all_address,))
+        #send_message(clientsocket, 'Connection Established') 
+        Access_response = Grant_Chat_Access(clientsocket, address)
+        if Access_response[0] is True:
+            online_users[Access_response[1]] = {
+                'Address' : address,
+                'Socket' : clientsocket
+            }
+            send_message(clientsocket, True)
+            print("Calling this Thread")
+            Thread(target=listen_from_clients, args=(clientsocket,online_users,)).start()
+        else:
+            send_message(clientsocket, False)
+            clientsocket.close()
+        
 
 
 def GenerateKey():
@@ -54,6 +146,6 @@ if __name__ == "__main__":
     server.listen(10)
     print('Server has been started')
     print('Waiting for connections')
-    Recieve_thread = Thread(target=accept_incoming_connections, args=(server,all_address,))
+    Recieve_thread = Thread(target=accept_incoming_connections, args=(server,online_users,))
     Recieve_thread.start()
     Recieve_thread.join()
